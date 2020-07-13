ccf74fa4cc1d4b2f8b17a1b71825b08fc960ffb8
sakthi77 <67312875+sakthi77@users.noreply.github.com>
2020-07-13 12:10:49 +0530

    Update speakgoodbye.js

--- module4/speakgoodbye.js
+++ module4/speakgoodbye.js
@@ -17,7 +17,6 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak=function (name) {
-  byeSpeaker.name=name;
   console.log(speakWord + " " + byeSpeaker.name);
 }
 
